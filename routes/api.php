a574f8c7b19df24a141dcd9e5e73c615eab203de
reza <bghavipor@gmail.com>
2023-11-14 20:53:02 +0330

    add event create method

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\AlarmController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Api\RegisterController;
 use \App\Http\Controllers\Api\LoginController;
 use \App\Http\Controllers\Api\PetController;
 use \App\Http\Controllers\Api\BreedController;
+use \App\Http\Controllers\Api\EventController;
+use App\Models\Alarm;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -35,5 +39,19 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::prefix('breed')->name('breed')->group(function () {
         Route::get('/', [BreedController::class, 'all'])->name('all');
+    });
+
+
+    Route::prefix('alarm')->name('alarm')->group(function () {
+        Route::get('/', [AlarmController::class, 'all'])->name('all');
+        Route::get('/{alarm}', [AlarmController::class, 'show'])->name('show');
+        Route::post('/', [AlarmController::class, 'create'])->name('create');
+        Route::patch('/{alarm}', [AlarmController::class, 'update'])->name('update');
+        Route::delete('/{alarm}', [AlarmController::class, 'delete'])->name('delete');
+    });
+
+    Route::prefix('event')->name('event')->group(function () {
+        Route::get('/', [\App\Http\Controllers\Api\EventController::class, 'get'])->name('get');
+        Route::post('/' , [EventController::class, 'create'])->name('create');
     });
 });
