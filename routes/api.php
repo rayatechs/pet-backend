240f83b42f54638b5156a76304b4446d72f28fc3
reza <bghavipor@gmail.com>
2023-11-24 12:06:37 +0330

    add user api method show and ypdate

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use \App\Http\Controllers\Api\LoginController;
 use \App\Http\Controllers\Api\PetController;
 use \App\Http\Controllers\Api\BreedController;
 use \App\Http\Controllers\Api\EventController;
+use App\Http\Controllers\Api\UserController;
 use App\Models\Alarm;
 
 /*
@@ -34,6 +35,11 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResources([
         'alarm' => AlarmController::class,
         'pet'   => PetController::class,
+
+    ]);
+
+    Route::resource('user', UserController::class)->only([
+        'show', 'update'
     ]);
 
     Route::post('pet/{pet}/upload-avatar', [PetController::class, 'uploadAvatar'])->name('pet.upload.avatar');
@@ -43,6 +49,6 @@ Route::middleware('auth:sanctum')->group(function () {
     });
     Route::prefix('event')->name('event')->group(function () {
         Route::get('/', [\App\Http\Controllers\Api\EventController::class, 'get'])->name('get');
-        Route::post('/' , [EventController::class, 'create'])->name('create');
+        Route::post('/', [EventController::class, 'create'])->name('create');
     });
 });
